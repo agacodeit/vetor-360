3ae640648a88524b8e89168d5c3d657d402fcb15
andressa.ieque <andressa.ieque@agacode.com.br>
2025-10-03 13:17:53 -0300

    Adiciona o pacote Font Awesome e implementa novo layout na página do dashboard, incluindo mensagem de boas-vindas e botão para criar solicitações. Estiliza a página com flexbox para melhor apresentação.

--- src/app/pages/authorized/dashboard/dashboard.ts
+++ src/app/pages/authorized/dashboard/dashboard.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonComponent
   ],
+  standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
 
+
+  openCreateSolicitationModal() {
+
+  }
 }
