9ea87d0fc99e22a9168c0e5bf7355bc3d1f06d3b
andressa.ieque <andressa.ieque@agacode.com.br>
2025-10-03 13:23:26 -0300

    Implementa modal na página do dashboard, adiciona lógica para abertura do modal e ajusta estilos com flexbox para melhor apresentação. Modifica a estrutura do componente para incluir o novo modal e atualiza o layout da página.

--- src/app/pages/authorized/dashboard/dashboard.ts
+++ src/app/pages/authorized/dashboard/dashboard.ts
@@ -1,21 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../../shared';
+import { ButtonComponent, ModalComponent, ModalService } from '../../../shared';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CommonModule,
-    ButtonComponent
+    ButtonComponent,
+    ModalComponent
   ],
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
-
+  
+  constructor(private modalService: ModalService) {}
 
   openCreateSolicitationModal() {
+    this.modalService.open({
+      id: "example-modal",
+      title: "Meu Modal",
+      size: "md",
+      showHeader: true,
+      showCloseButton: true,
+      closeOnBackdropClick: true,
+      closeOnEscapeKey: true,
+    });
 
   }
 }
