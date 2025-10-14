105fc6c0fa4258c95c8425659ecd3ab531561c26
P0659387 <cprpc@hotmail.com>
2025-10-14 10:49:35 -0300

    Update SCSS styles for improved typography and layout consistency. Adjust font sizes in various components and enhance header styles with updated padding and logo dimensions. Refresh TypeScript build info and replace logo image with a new version.

--- src/app/pages/authorized/authorized.ts
+++ src/app/pages/authorized/authorized.ts
@@ -1,28 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent, MessageContainerComponent } from '../../shared';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../shared';
 import { AuthService } from '../../shared/services/auth/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authorized',
   imports: [
     RouterOutlet,
-    HeaderComponent,
-    MessageContainerComponent
+    HeaderComponent
   ],
   templateUrl: './authorized.html',
   styleUrl: './authorized.scss'
 })
 export class Authorized {
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
+  onProfileClick() { }
 
-  onProfileClick() {
-
-
-  }
 }
