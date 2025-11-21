3e558db2803ac49878ca751dda98be806ac6cf31
Lucas Gomes <cprpc@hotmail.com>
2025-11-21 10:34:43 -0300

    feat: enhance app initialization and user loading
    - Refactored app configuration to utilize Angular's dependency injection for preloading operation types and initializing the user.
    - Added `initializeUser` method in AuthService to load the logged-in user before application startup, improving user experience and authentication flow.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideAppInitializer, inject } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './shared/interceptors';
-import { OperationRegistryService } from './shared';
+import { OperationRegistryService, AuthService } from './shared';
 
-function preloadOperationTypes(operationRegistry: OperationRegistryService) {
-  return () => operationRegistry.loadOperationTypes();
+function preloadOperationTypes() {
+  const operationRegistry = inject(OperationRegistryService);
+  return operationRegistry.loadOperationTypes();
+}
+
+function initializeUser() {
+  const authService = inject(AuthService);
+  return authService.initializeUser();
 }
 
 export const appConfig: ApplicationConfig = {
@@ -15,12 +21,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimationsAsync('animations'),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: preloadOperationTypes,
-      deps: [OperationRegistryService],
-      multi: true
-    },
+    provideAppInitializer(preloadOperationTypes),
+    provideAppInitializer(initializeUser),
     provideRouter(routes),
   ]
 };
